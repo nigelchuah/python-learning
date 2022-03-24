f6cb34479798fc354fc8fadcdf0984ea380ee90e
Nigel Chuah <101927877+nigelcws@users.noreply.github.com>
2022-03-24 00:28:26 +0000

    Add files via upload

---
+++ pycon2018/lists.py
@@ -0,0 +1,75 @@
+"""List comprehension exercises"""
+import math
+
+def get_vowel_names(names):
+    """Return a list containing all names given that start with a vowel."""
+    return [
+        name 
+        for name in names 
+        if name[0].upper() in "AEIOU"
+        ]
+
+
+def power_list(numbers):
+    """Return a list that contains each number raised to the i-th power."""
+    return [x**i 
+            for i, x in enumerate(numbers)
+            ]
+
+
+def flatten(matrix):
+    """Return a flattened version of the given 2-D matrix (list-of-lists)."""
+    return [
+        number 
+        for lists in matrix 
+        for number in lists
+        ] 
+
+
+def reverse_difference(x):
+    """Return list subtracted from the reverse of itself."""
+    return[
+        x[i] - x[-i-1]
+        for i in range(len(x))
+    ]
+
+def matrix_add(matrix1, matrix2):
+    """Add corresponding numbers in given 2-D matrices."""
+    return [
+        [num1 + num2 for num1, num2, in zip(row1, row2)]
+        for row1, row2 in zip(matrix1, matrix2)
+    ]
+
+
+def transpose(matrix):
+    """Return a transposed version of given list of lists."""
+    return [  
+        [row[i] for row in matrix]
+        for i in range(len(matrix))
+    ]
+
+    # return [[matrix[i][j] for i in range(len(matrix))] for j in range(len(matrix[0]))]
+
+# matrix = [['a','b','c'],['d','e','f'],['g','h','i']]
+# print(transpose(matrix))
+
+def get_factors(N):
+    """Return a list of all factors of the given number."""
+    return [n for n in range(1, N+1)  if N%n == 0]
+
+def triples(N):
+    """Return list of Pythagorean triples less than input num."""
+    
+    return [
+        (i, j, int(math.sqrt(i**2 + j**2)))
+        for i in range(1,N)
+        for j in range(i,N)
+        if math.sqrt(i**2 + j**2)%1 == 0 and math.sqrt(i**2 + j**2) < N
+    ]
+    # trips = []
+    # for i in range(1,N):
+    #     for j in range(i,N):
+    #         if math.sqrt(i**2 + j**2)%1 == 0 and math.sqrt(i**2 + j**2) < N:
+    #             trips.append((i, j, int(math.sqrt(i**2 + j**2))))
+            
+    # return trips
